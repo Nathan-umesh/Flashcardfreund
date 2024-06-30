4131b2b90d17371915232480f26e08e71cb2c497
Nathan Umesh <nathan.umesh43@gmail.com>
2024-06-30 09:57:39 +0200

    centered toolbar & stacked widget w/o functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,33 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->toolBar->setMovable(false);
+
+    auto tbar_widget = new QWidget(this);
+    auto tbar_layout = new QHBoxLayout(tbar_widget);
+
+    auto tbar_left_spacer = new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    tbar_layout->addItem(tbar_left_spacer);
+
+    auto tbarbutton_path = new QToolButton();
+    tbarbutton_path->setText("Path");
+    tbarbutton_path->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Minimum);
+    tbar_layout->addWidget(tbarbutton_path);
+
+    auto tbar_middle_spacer = new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    tbar_layout->addItem(tbar_middle_spacer);
+
+    auto tbarbutton_flashcard = new QToolButton();
+    tbarbutton_flashcard->setText("Flashcards");
+    tbarbutton_flashcard->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Minimum);
+    tbar_layout->addWidget(tbarbutton_flashcard);
+
+    auto tbar_right_spacer = new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    tbar_layout->addItem(tbar_right_spacer);
+
+    tbar_widget->setLayout(tbar_layout);
+
+    ui->toolBar->addWidget(tbar_widget);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -1,8 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
-
+#include <QToolButton>
+/*
+#include <QHBoxLayout>
+#include <QWidget>
+#include <QSpacerItem>
+#include <QSizePolicy>
+*/
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
